cc26e7ba524fc91ea987fa681bece742459e2846
shilangyu <xmarcinmarcin@gmail.com>
2024-12-02 21:23:14 +0100

    Add day 1

--- AoC2024/Day01.lean
+++ AoC2024/Day01.lean
@@ -1,1 +1,31 @@
-def hello := "world"
+-- Standard definition of the absolute value
+def Nat.abs (x y : Nat) := if x > y then x - y else y - x
+-- Alternative definition using the fact that subtraction is saturated for natural numbers, that is, x - y = 0 if x ≤ y
+def Nat.dist (x y : Nat) := (x - y) + (y - x)
+
+-- We prove that the two definitions are equivalent
+theorem abs_eq_dist : Nat.abs = Nat.dist :=
+  by
+    -- We prove the equality by extensionality (axiom in Lean I think?)
+    funext x y
+    unfold Nat.abs Nat.dist
+    split -- consider the two cases x > y and ¬(x > y)
+    · case _ h =>
+      have h : (y <= x) := Nat.le_of_succ_le h
+      simp
+      exact Nat.sub_eq_zero_of_le h
+    · case _ h =>
+      simp at *
+      exact Nat.sub_eq_zero_of_le h
+
+-- Counting the number of elements in a list is equivalent to the length of a list filtered by that number
+theorem count_eq_length_filter (l : List Nat) (x : Nat) : l.count x = (l.filter (Eq x)).length :=
+  by
+    induction l with
+    | nil =>
+      simp
+    | cons hd tl ih =>
+      -- We consider the two cases hd = x and hd ≠ x
+      by_cases h : x = hd
+      rw [h] at ih
+      all_goals { simp [h]; exact ih }
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # AoC-2024
+
+Advent of Code 2024 where each day contains a formal proof about something related to the task.
+
+Occasionally may also contain actual solutions.
+
+## Day 1
+
+Using the fact that subtraction on natural numbers is saturated in Lean, we prove that the standard absolute function implementation
+
+```lean
+if x > y then x - y else y - x
+```
+
+is equivalent to
+
+```lean
+(x - y) + (y - x)
+```
+
+Then we note that a counting function over a list is the same as taking the length of a filtered list.
+
+```lean
+l.count x = (l.filter (Eq x)).length
+```
